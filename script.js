e4dab4195acbf6b14e8d93ef24fc39d22f83dc48
shazidhasanriam <shazid.riam@northsouth.edu>
2021-12-07 13:35:34 +0600

    Done With The Project

--- script.js
+++ script.js
@@ -6,17 +6,42 @@ const message = document.getElementById("message");
 
 let isValid = false;
 
+let passwordMatch = false;
+
 function validateForm() {
   // Using constraint API
   isValid = form.checkValidity();
   //   console.log(isValid);
   //   Error Message
-  message.textContent = "Please fill out all the fields.";
-  message.style.color = "red";
-  messageContainer.style.borderColor = "red";
+  if (!isValid) {
+    message.textContent = "Please Fill Out All The Fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+  // Password Match check
+  if (createPasswordEl.value === confirmPasswordEl.value) {
+    passwordMatch = true;
+    createPasswordEl.style.borderColor = "green";
+    confirmPasswordEl.style.borderColor = "green";
+  } else {
+    passwordMatch = false;
+    message.textContent = "Please Make Sure Your Passwords Match";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    createPasswordEl.style.borderColor = "red";
+    confirmPasswordEl.style.borderColor = "red";
+    return;
+  }
+  // valid form and passwords matches
+  if (isValid && passwordMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
 }
 
-function handleFormDate(e) {
+function handleFormData(e) {
   e.preventDefault();
   //   console.log(e);
   // Validate Form
@@ -24,4 +49,4 @@ function handleFormDate(e) {
 }
 
 // Event Listener
-form.addEventListener("submit", handleFormDate);
+form.addEventListener("submit", handleFormData);
